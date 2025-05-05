8c211809079a0c700858363e7ddbdfa1929d4605
Sabih <sabih.ulhasan@yahoo.com>
2025-05-05 10:38:49 +0200

    Project Created

---
+++ Hush Alert/Hush Alert/Hush_AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hush_AlertApp.swift
+//  Hush Alert
+//
+//  Created by Muhammad Sabihul Hasan on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Hush_AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
